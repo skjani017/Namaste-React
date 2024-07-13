36f21c056f2f8ec3960a72f357d55810f619dd4b
janis <janishaik1702@gmail.com>
2024-07-14 00:17:27 +0530

    Igniting our App

--- App.js
+++ App.js
@@ -1,42 +1,21 @@
-{
-  /* <div id="parent">
-    <div id= "child">
-        <h1>i am h1 tag</h1>
-    </div>
-
-     <div id= "child2">
-        <h1>i am h1 tag</h1>
-    </div>
-</div> */
-}
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am h1 tag "),
+    React.createElement("h1", {}, "NAMASTE REACT "),
     React.createElement("h2", {}, "i am h2 tag "),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "i am h1 tag "),
     React.createElement("h2", {}, "i am h2 tag "),
   ])
-
 );
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "root" },
-//   "hello world from react!"
-// );
 
 console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-#Namaste-React:
+# Namaste-React:
+
+..................................
+
+# parcel:
+
+- Dev Bulind
+- local server
+- HMR : Hot Module Replacement
+- Flie Watching Algorithm - write in c++
+- caching - Faster bulids
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Heshing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree shaking - remove unused code
+- Different dev production
